49109f6404a69c9dfdc7ef78bbe0b676e44bafba
vonmoraes <31371849+vonmoraes@users.noreply.github.com>
2018-03-19 18:57:55 -0300

    0.0.0

--- app/src/main/java/com/example/morae/mycontacts/Pessoas.java
+++ app/src/main/java/com/example/morae/mycontacts/Pessoas.java
@@ -4,5 +4,57 @@ package com.example.morae.mycontacts;
  * @author Lucas de Souza Moraes
  */
 public class Pessoas {
-    
+    String nome;
+    String endereco;
+    String referencia;
+    String fone;
+    String email;
+
+    public Pessoas(String nome, String endereco, String referencia, String fone, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.referencia = referencia;
+        this.fone = fone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
